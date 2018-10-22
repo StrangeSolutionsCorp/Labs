9dbe1552784f74c51e856bfade10bd84a58e8d2d
StrangeSolutionsCorp <43315596+StrangeSolutionsCorp@users.noreply.github.com>
2018-10-22 15:27:02 +0300

    Update 9.c

--- 9.c
+++ 9.c
@@ -12,17 +12,23 @@ int main()
 	char *end;
 	char *begin;
 	char c;
-	int flag;
-	int len; 
-	int word; 
+	int flag; // признак слова 
+	int len; // длина слова 
+	int word; // соответствие параметрам слова
 	char *dst;
 	char *src;
+	int i;
 
 	in = fopen("IN.txt", "r");
 	out = fopen("OUT.txt", "w");
 	while (feof(in)==0)
 	{
-		fgets(line, MAXLINE, in);
+
+		if (fgets(line, MAXLINE, in) == NULL)
+		{
+			fprintf(out,"File was Empty\n");
+			return 0;
+		}
 		ptr = line;
 		begin = line;
 		end = line;
@@ -31,35 +37,37 @@ int main()
 		len = 0;
 		do
 		{
-			c = *ptr; 
-			if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0') 
+			c = *ptr; // взять текущий символ из буфера 
+			if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0') // найден разделитель 
 			{
-				if ((len % 2 == 0) && (word == YES)) 
+				if ((len % 2 == 0) && (word == YES)) // длина больше нужной и это именно слово
 				{
-					end = ptr - 1;  
+					end = ptr - 1; // запомнить указатель на конец слова 
 					src = end + 1;
 					dst = begin;
-					while ((*dst++ = *src++) != '\0');  
-					ptr = begin; 
+					while ((*dst++ = *src++) != '\0'); // удаление слова (перенос оставшейся части строки) 
+					ptr = begin; // начать обработку строки с перенесённого фрагмента 
 				}
 				flag = NO;
 				word = NO;
 				len = 0;
 			}
-			else  
+			else // найдена буква 
 			{
-				if ((c >= '0') && (c <= '9')) 
-					word = NO; 
+				if ((c >= '0') && (c <= '9')) // если цифра
+					word = NO; // это не слово
 				if (flag == NO)
 				{
-					begin = ptr; 
+					begin = ptr; // запомнить указатель на начало слова 
 					word = YES;
 				}
 				flag = YES;
 				len++;
 			}
-			ptr++; 
+			ptr++; // продвинуть вперёд указатель на текущий символ 
+			 // увеличить длину слова 
 		} while (c != '\0');
 		fputs(line, out);
 	}
+	return 0;
 }
